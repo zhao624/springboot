c4e6ea80dbe75a09301d81abfb4d5205e668a4ff
zhaoxugui <zxg184072115@outlook.com>
2017-11-17 13:20:49 +0800

    'null'

---
+++ src/main/java/com/zxg/util/cookie/DateUtil.java
@@ -0,0 +1,58 @@
+package com.zxg.util.cookie;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateUtil {
+
+	/**
+	 * date2比date1多的天数
+	 * 
+	 * @param date1
+	 * @param date2
+	 * @return
+	 */
+	public static int differentDays(Date date1, Date date2) {
+		Calendar cal1 = Calendar.getInstance();
+		cal1.setTime(date1);
+
+		Calendar cal2 = Calendar.getInstance();
+		cal2.setTime(date2);
+		int day1 = cal1.get(Calendar.DAY_OF_YEAR);
+		int day2 = cal2.get(Calendar.DAY_OF_YEAR);
+
+		int year1 = cal1.get(Calendar.YEAR);
+		int year2 = cal2.get(Calendar.YEAR);
+		if (year1 != year2) // 同一年
+		{
+			int timeDistance = 0;
+			for (int i = year1; i < year2; i++) {
+				if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) // 闰年
+				{
+					timeDistance += 366;
+				} else // 不是闰年
+				{
+					timeDistance += 365;
+				}
+			}
+
+			return timeDistance + (day2 - day1);
+		} else // 不同年
+		{
+			System.out.println("判断day2 - day1 : " + (day2 - day1));
+			return day2 - day1;
+		}
+	}
+	
+	
+	/**
+     * 通过时间秒毫秒数判断两个时间的间隔
+     * @param date1
+     * @param date2
+     * @return
+     */
+    public static int differentDaysByMillisecond(Date date1,Date date2){
+        int days = (int) ((date2.getTime() - date1.getTime()) / (1000*3600*24));
+        return days;
+    }
+}
