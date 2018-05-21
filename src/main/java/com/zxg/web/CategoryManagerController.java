58a1ae0570855a9cc6366e790772365e99842cc0
zhaoxugui <zxg184072115@outlook.com>
2018-05-21 10:12:49 +0800

    'addlogbackconfig'

--- src/main/java/com/zxg/web/CategoryManagerController.java
+++ src/main/java/com/zxg/web/CategoryManagerController.java
@@ -3,13 +3,14 @@ package com.zxg.web;
 import java.util.List;
 
 import com.zxg.mapper.ICategoryMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.zxg.bean.Category;
-
+@Slf4j
 @Controller
 @RequestMapping("/admin")
 public class CategoryManagerController {
@@ -19,6 +20,7 @@ public class CategoryManagerController {
 
     @RequestMapping(value = "/category")
     public ModelAndView toAssortment() {
+        log.debug("toCategory....");
         ModelAndView modelAndView = new ModelAndView();
         List<Category> category = categoryMapper.quertAllCategory();
         modelAndView.addObject("category", category);
